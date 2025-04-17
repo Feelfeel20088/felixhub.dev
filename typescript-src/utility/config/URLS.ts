2a860f534b04886acbffed0921928e871d561db4
Felix Vujasin <feelfeel200088@gmail.com>
2025-04-17 02:34:30 +0000

    cluster ip url changes

--- typescript-src/utility/config/URLS.ts
+++ typescript-src/utility/config/URLS.ts
@@ -1,7 +1,7 @@
 export default new class URLS {
 
     // ollama
-    ollama_internal = 'http://ollama-internal.utility.svc.cluster.local:11434/api/chat';
+    ollama_internal = 'http://ollama.ollama.svc.cluster.local:11434/api/chat';
     ollama_external = 'http://192.168.70.20:30000/api/chat';
     ollama_local = 'http://localhost:11434/api/chat';
 
@@ -10,7 +10,7 @@ export default new class URLS {
 
     // kahootbot
     // swarm endpoint: creates a swarm
-    kahootbot_internal = 'http://just-another-kahoot-bot-internal.utility.svc.cluster.local:8000/swarm';
+    kahootbot_internal = 'http://just-another-kahoot-bot-internal.just-another-kahoot-bot.svc.cluster.local:8000/swarm';
     kahootbot_external = 'http://192.168.70.20:30005/swarm';
     kahootbot_local = 'http://127.0.0.1:8000/swarm'
 };
