be240f36263aed1941f5b0af6b779f34e0d5637d
Felix <feelfeel200088@gmail.com>
2025-06-24 10:06:52 -0500

    i should REALLY have a dev branch

--- flake.nix
+++ flake.nix
@@ -26,10 +26,12 @@
 
             fromImage = pkgs.dockerTools.pullImage {
               imageName = "paketobuildpacks/nodejs";
+              finalImageName = "paketobuildpacks/nodejs";
               finalImageTag = "latest";
-              sha256 = "sha256-1VxV9ibVHHN9ABqDb0da9O2B6aiZipUlR3KUnhOkfnM=";
-              imageDigest = "sha256-1VxV9ibVHHN9ABqDb0da9O2B6aiZipUlR3KUnhOkfnM=";
+              imageDigest = "sha256:8aaa7ef831b72dce5cfff67e5eaa651804fe43359a66c71c226651fc834ff53b";
+              sha256 = "1VxV9ibVHHN9ABqDb0da9O2B6aiZipUlR3KUnhOkfnM=";
             };
+
             config = {
               Cmd = [ "npm" "start" ];
               WorkingDir = "/app";
