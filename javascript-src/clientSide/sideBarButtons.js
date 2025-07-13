50fd4900f1817e40194a229b3ee3767c15cd698e
felix <feelfeel200088@gmail.com>
2025-07-13 13:40:39 -0500

    rename typescript crap to src

--- javascript-src/clientSide/sideBarButtons.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-document.getElementById('open-btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.add('open');
-});
-document.getElementById('close-btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.remove('open');
-});
--- javascript-src/utility/FelixHubErrorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export default abstract class FelixHubErrorHandler implements FelixHubServiceBase  {
-//     utils!: Utils; // Instance of Utils, initialized later
-//     server!: FastifyInstance; // Instance of Fastify, initialized late
-//     params?: Record<string, any>; // Custom parameters passed to the service
-//     abstract callBack(err: FastifyError, req: FastifyRequest, reply: FastifyReply): void;
-// }
